014b137801629b06b5aa7f8beb2631f1b9bbfd38
Franco Alvarez Muñoz <falvarez.munoz@hotmail.es>
2024-09-04 15:53:43 +0000

    finished

--- src/js/component/AudioPlayer.jsx
+++ src/js/component/AudioPlayer.jsx
@@ -9,16 +9,56 @@ const AudioPlayer = () => {
     const [time, setTime] = useState(0)
     const [displayTime, setDisplayTime] = useState(0)
     const [duration, setDuration] = useState(0)
+    const [playList, setPlaylist] = useState([])
+
+    /*     const playList = [
+            { "id": 1, "name": "Mario Castle", "url": "/sound/files/mario/songs/castle.mp3", "category": "category" },
+            { "id": 2, "name": "Mario Star", "url": "/sound/files/mario/songs/hurry-starman.mp3", "category": "category" },
+            { "id": 3, "name": "Mario Overworld", "url": "/sound/files/mario/songs/overworld.mp3", "category": "category" },
+            { "id": 4, "name": "Mario Stage 1", "url": "/sound/files/mario/songs/stage1.mp3", "category": "category" },
+            { "id": 5, "name": "Mario Stage 2", "url": "/sound/files/mario/songs/stage2.mp3", "category": "category" },
+            { "id": 6, "name": "Mario Star", "url": "/sound/files/mario/songs/starman.mp3", "category": "category" },
+            { "id": 7, "name": "Mario Underworld", "url": "/sound/files/mario/songs/underworld.mp3", "category": "category" },
+            { "id": 8, "name": "Mario Underwater", "url": "/sound/files/mario/songs/underwater.mp3", "category": "category" },
+            { "id": 9, "name": "Zelda Castle", "url": "/sound/files/videogame/songs/zelda_castle.mp3", "category": "category" },
+            { "id": 10, "name": "Zelda Outworld", "url": "/sound/files/videogame/songs/zelda_outworld.mp3", "category": "category" },
+            { "id": 11, "name": "Zelda Titles", "url": "/sound/files/videogame/songs/zelda_title.mp3", "category": "category" },
+            { "id": 12, "name": "Sonic Brain Zone", "url": "/sound/files/videogame/songs/sonic_brain-zone.mp3", "category": "category" },
+            { "id": 13, "name": "Zelda Link To Past", "url": "/sound/files/videogame/songs/zelda_link-to-past.mp3", "category": "category" },
+            { "id": 14, "name": "Flintstones", "url": "/sound/files/cartoons/songs/flintstones.mp3", "category": "cartoon" },
+            { "id": 15, "name": "power-rangers", "url": "/sound/files/cartoons/songs/power-rangers.mp3", "category": "cartoon" },
+            { "id": 16, "name": "simpsons", "url": "/sound/files/cartoons/songs/simpsons.mp3", "category": "cartoon" },
+            { "id": 17, "name": "south-park", "url": "/sound/files/cartoons/songs/south-park.mp3", "category": "cartoon" },
+            { "id": 18, "name": "thundercats", "url": "/sound/files/cartoons/songs/thundercats.mp3", "category": "cartoon" },
+            { "id": 19, "name": "x-men", "url": "/sound/files/cartoons/songs/x-men.mp3", "category": "cartoon" }
+        ];
+     */
+
+
+    const updatePLaylist = () => {
+        fetch("https://playground.4geeks.com/sound/songs")
+            .then((response) => {
+                return response.json()
+            })
+            .then((newResponse) => {
+                console.log(newResponse)
+                setPlaylist(newResponse.songs)
+            })
+    }
+
+    const updateDisplayTime = (e) => {
+        // setDisplayTime(Math.floor(songRef?.current?.currentTime));
+        setDisplayTime(Math.floor(e.target.currentTime));
+    };
 
     useEffect(() => {
-        const updateDisplayTime = () => {
-            setDisplayTime(Math.floor(songRef.current.currentTime));
-        };
-        songRef.current.addEventListener('timeupdate', updateDisplayTime);
+        updatePLaylist()
+
+        /* songRef.current.addEventListener('timeupdate', updateDisplayTime);
         return () => {
             songRef.current.removeEventListener('timeupdate', updateDisplayTime);
-        };
-    });
+        }; */
+    }, []);
 
     const playControl = () => {
         currentIndex !== undefined && (play ? songRef.current.pause() : songRef.current.play())
@@ -46,35 +86,14 @@ const AudioPlayer = () => {
     const sliderTimeChanger = (event) => {
         if (currentIndex !== undefined) {
             const newTime = event.target.value;
-            songRef.current.currentTime = newTime;
             setTime(newTime);
+            songRef.current.currentTime = time;
         }
     }
 
-    const playList = [
-        { "id": 1, "name": "Mario Castle", "url": "/sound/files/mario/songs/castle.mp3", "category": "category" },
-        { "id": 2, "name": "Mario Star", "url": "/sound/files/mario/songs/hurry-starman.mp3", "category": "category" },
-        { "id": 3, "name": "Mario Overworld", "url": "/sound/files/mario/songs/overworld.mp3", "category": "category" },
-        { "id": 4, "name": "Mario Stage 1", "url": "/sound/files/mario/songs/stage1.mp3", "category": "category" },
-        { "id": 5, "name": "Mario Stage 2", "url": "/sound/files/mario/songs/stage2.mp3", "category": "category" },
-        { "id": 6, "name": "Mario Star", "url": "/sound/files/mario/songs/starman.mp3", "category": "category" },
-        { "id": 7, "name": "Mario Underworld", "url": "/sound/files/mario/songs/underworld.mp3", "category": "category" },
-        { "id": 8, "name": "Mario Underwater", "url": "/sound/files/mario/songs/underwater.mp3", "category": "category" },
-        { "id": 9, "name": "Zelda Castle", "url": "/sound/files/videogame/songs/zelda_castle.mp3", "category": "category" },
-        { "id": 10, "name": "Zelda Outworld", "url": "/sound/files/videogame/songs/zelda_outworld.mp3", "category": "category" },
-        { "id": 11, "name": "Zelda Titles", "url": "/sound/files/videogame/songs/zelda_title.mp3", "category": "category" },
-        { "id": 12, "name": "Sonic Brain Zone", "url": "/sound/files/videogame/songs/sonic_brain-zone.mp3", "category": "category" },
-        { "id": 13, "name": "Zelda Link To Past", "url": "/sound/files/videogame/songs/zelda_link-to-past.mp3", "category": "category" },
-        { "id": 14, "name": "Flintstones", "url": "/sound/files/cartoons/songs/flintstones.mp3", "category": "cartoon" },
-        { "id": 15, "name": "power-rangers", "url": "/sound/files/cartoons/songs/power-rangers.mp3", "category": "cartoon" },
-        { "id": 16, "name": "simpsons", "url": "/sound/files/cartoons/songs/simpsons.mp3", "category": "cartoon" },
-        { "id": 17, "name": "south-park", "url": "/sound/files/cartoons/songs/south-park.mp3", "category": "cartoon" },
-        { "id": 18, "name": "thundercats", "url": "/sound/files/cartoons/songs/thundercats.mp3", "category": "cartoon" },
-        { "id": 19, "name": "x-men", "url": "/sound/files/cartoons/songs/x-men.mp3", "category": "cartoon" }
-    ];
 
     const domList = <ul className="pointerHover list-group">
-        {playList.map((item, index) => (
+        {!!playList && playList.map((item, index) => (
             <li className="list-group-item bg-dark text-white border-secondary" onClick={() => changeRef(item, index)} key={index} >
                 {currentIndex == index && (!play ? (
                     <i onClick={() => playControl()} className="fa-solid fa-pause"></i>
@@ -88,8 +107,10 @@ const AudioPlayer = () => {
     return (
         <div>
             {domList}
-            <audio ref={songRef} src={songRef.src} autoPlay onPlay={() => setPlay(true)} onPause={() => setPlay(false)}
-                onEnded={() => songEnded()} onLoadedMetadata={() => setDuration(Math.round(songRef.current.duration))} />
+
+            <audio onTimeUpdate={(e) => updateDisplayTime(e)} ref={songRef} src={songRef.src} autoPlay onPlay={() => setPlay(true)} onPause={() => setPlay(false)}
+                onEnded={() => songEnded()} onLoadedMetadata={(e) => setDuration(Math.round(e.target.duration))} />
+
             <div className="playerBar fixed-bottom bg-dark pt-3">
                 <div className="d-flex justify-content-center align-items-center">
                     <p className="text-white align-self-center my-0 mx-2" >
